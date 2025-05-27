8f7d2818d77bd93fd178f9eba080719668c9df4e
Saniya1976 <saniyakumari1976@gmail.com>
2025-05-27 16:15:29 +0530

    crud operations

--- server.js
+++ server.js
@@ -1,44 +1,70 @@
 const express = require('express');
 const morgan = require('morgan');
-
-
 const mongoose = require('mongoose');
 const User = require('./models/user');
-
-const dbconnection=require('./config/db'); // Assuming you have a db.js file for database connection
+const dbconnection=require('./config/db'); 
 const server = express();
-
-// Built-in middleware to parse JSON bodies
 server.use(express.json());
-
-// Built-in middleware to parse URL-encoded bodies (like form submissions)
 server.use(express.urlencoded({ extended: true }));
 
-// Third-party middleware for logging HTTP requests
 server.use(morgan('dev'));
+server.set('view engine', 'ejs');
 
-// Custom middleware example: logs the current timestamp for every request
 server.use((req, res, next) => {
   console.log("hello babe this is middleware");
-  next(); // pass control to the next middleware or route handler
+  next(); 
 });
 
-// Root route handler
 server.get('/', (req, res) => {
   res.send('hello world');
 });
 
-// About page route handler
 server.get('/about', (req, res) => {
   res.send('hello babe this is about page');
 });
-
-// 404 Middleware (catch-all for undefined routes)
+server.get('/register',(req,res)=>{
+  res.render('register');
+})
+// Register route to handle user registration
+//CRUD operation
+//CREATE
+server.post("/register", async (req, res) => {
+  const {name,email,password}=req.body;
+ const newuser= await User.create({
+    name:name,
+    email:email,
+    password:password
+  });
+ 
+  console.log(req.body);
+  res.send(newuser);
+ 
+});
+//READ
+ server.get('/getusers',  (req, res) => {
+    User.find().then((users)=>{
+      res.send(users);
+    })
+  });
+  //UPDATE
+  server.get('/updateuser',async(req,res)=>{
+   await User.findOneAndUpdate({name:'Saniya'}, {name:'Saniya Khan'}, {new:true})
+    .then((user) => {
+      res.send(user);
+    })
+  })
+//DELETE
+server.get('/deleteuser',async(req,res)=>{
+  await User.findOneAndDelete({name:'cutie'})
+    .then((user) => {
+      res.send(user);
+    })
+})
 server.use((req, res) => {
   res.status(404).send('Page not found');
 });
 
-// Error-handling middleware (for any errors thrown in routes/middleware)
+
 server.use((err, req, res, next) => {
   console.error('Error:', err.stack);
   res.status(500).send('Something broke!');
